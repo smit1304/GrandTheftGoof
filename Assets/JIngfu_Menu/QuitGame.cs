727f1bbf74999400040f5e8b563681e70e040291
jingfuLuo <Lluojingfu@gmail.com>
2025-12-02 11:51:15 -0500

    Add Game Over Menu

---
+++ Assets/JIngfu_Menu/QuitGame.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    
+    public void QuitGames()
+    {
+#if UNITY_EDITOR
+        
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+       
+        Application.Quit();
+#endif
+    }
+}
